17d65a798bed77e38c3ddf67e75f1a029a3da44a
kartikgoyal123 <74776059+kartikgoyal123@users.noreply.github.com>
2021-02-10 16:49:07 +0530

    Add files via upload

--- p5.play-boilerplate-master/sketch.js
+++ p5.play-boilerplate-master/sketch.js
@@ -1,10 +1,12 @@
 var hr,mn,sc;
+var scAngle,mnAngle,hrAngle;
 
 
 
 
 function setup() {
-  createCanvas(400,400);
+  createCanvas(500,500);
+  angleMode(DEGREES);
 }
 
 function draw() {
@@ -15,13 +17,32 @@ function draw() {
  sc = second();
 
  scAngle = map(sc,0,60,0,360);
+ mnAngle = map(mn,0,60,0,360);
+ hrAngle = map(hr,0,12,0,360);
 
+ translate(250,250);
  push();
  rotate(scAngle);
  stroke(255,0,0);
  strokeWeight(7);
- line(0,0,100,0);
+ line(0,0,200,0);
  pop();
  
+// translate(250,250);
+ push();
+ rotate(mnAngle);
+ stroke(0,255,0);
+ strokeWeight(7);
+ line(0,0,150,0);
+ pop();
+ 
+// translate(250,250);
+ push();
+ rotate(hrAngle);
+ stroke(0,0,255);
+ strokeWeight(7);
+ line(0,0,100,0);
+ pop();
+
   drawSprites();
 }
